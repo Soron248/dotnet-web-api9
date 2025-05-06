469846eec9e0f53f4e16348ee7616e7b8af1d4d8
Soron248 <iftekhar.soron@gmail.com>
2025-05-06 16:44:16 +0600

    Add /home route to web application
    This commit introduces a new GET endpoint at "/home" that returns the string "Your Home". Other application setup code, including HTTPS redirection, remains unchanged.

--- Program.cs
+++ Program.cs
@@ -1,12 +1,12 @@
 var builder = WebApplication.CreateBuilder(args);
 
-
-
 var app = builder.Build();
-
-
 
 app.UseHttpsRedirection();
 
+app.MapGet("/home", () =>
+{
+    return "Your Home";
+});
 
 app.Run();
