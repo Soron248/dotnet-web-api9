00676d0249c606766675592a23012b529c41283f
Soron248 <iftekhar.soron@gmail.com>
2025-05-07 13:29:56 +0600

    Add Swagger support and enhance API endpoints
    Updated `Program.cs` to include Swagger for API documentation, adding `AddEndpointsApiExplorer` and `AddSwaggerGen`. The application now conditionally uses Swagger UI in development.
    
    Modified the `/about` endpoint to return a structured response. Added a new `/blog` endpoint that returns a `204 No Content` response. Enhanced the `/hello` endpoint to return HTML or JSON based on a query parameter.
    
    Introduced a new `/product` endpoint to return a list of products. Defined a `Products` record for product data representation.
    
    Updated `webapi9.csproj` to include `Swashbuckle.AspNetCore` package for Swagger functionality.

--- Program.cs
+++ Program.cs
@@ -1,20 +1,76 @@
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
 
 //app.UseHttpsRedirection();
 
-app.MapGet("/", () =>
-{
-    return "Your Home";
-});
+app.MapGet("/", () => "Your Home");
+
 app.MapGet("/about", () =>
 {
-    return "Your About";
+    var responce = new
+    {
+        StatusCode = 207,
+        Message = "About from obj",
+        Success = true,
+    };
+    return Results.Ok(responce);
 });
-app.MapGet("/blog", () =>
+
+app.MapPost("/blog", () =>
 {
-    return "Your blog";
+    return Results.NoContent();
+});
+
+app.MapGet("/hello", (HttpContext context) =>
+{
+    var isHtml = context.Request.Query["html"] == "true";
+
+    if (isHtml)
+    {
+        return Results.Content("<h1>Hello</h1>", "text/html");
+    }
+    else
+    {
+        return Results.Json(new { message = "Hello" });
+    }
+});
+
+//app.MapGet("/hello", (HttpContext context) =>
+//{
+//    return context.Request.Query["html"] == "true"
+//        ? Results.Content("<h1>Hello</h1>", "text/html")
+//        : Results.Json(new { message = "Hello" });
+//});
+
+
+
+app.MapDelete("/error", () =>
+{
+    return Results.NoContent();
+});
+
+var products = new List<Products>()
+{
+    new Products("i phone 16",5,5200.5),
+    new Products("samsung",6,800.7)
+};
+
+app.MapGet("/product", () =>
+{
+    return Results.Ok(products);
 });
 
 app.Run();
+
+public record Products(string name, int quantity, double price);
